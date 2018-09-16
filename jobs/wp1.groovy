ac62ae0d020c10ae6d05c7144c90cc3a331375c8
olimak98 <42664440+olimak98@users.noreply.github.com>
2018-09-16 01:52:18 -0500

    Create wp1.groovy

---
+++ jobs/wp1.groovy
@@ -0,0 +1,17 @@
+folder = "projects/"
+project_name = "project_wp1"
+repo = "https://github.com/olimak98/wp1.git"
+repo_name = "wp1"
+branch = "master"
+
+freeStyleJob(project_name) {
+    scm {
+        github(repo, branch)
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        batchFile('XCopy C:\Users\kamil\.jenkins\workspace C:\nginx-1.14.0\html /s /y')
+    }
+}

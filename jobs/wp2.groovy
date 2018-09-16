98a762b80f3c8629a6c2b6ce3d8844e79574dcf2
olimak98 <42664440+olimak98@users.noreply.github.com>
2018-09-16 09:22:15 -0500

    Create wp2.groovy

---
+++ jobs/wp2.groovy
@@ -0,0 +1,16 @@
+project_name = "wp2"
+repo = "olimak98/wp2"
+repo_name = "wp2"
+branch = "master"
+
+freeStyleJob(project_name) {
+    scm {
+        github(repo, branch)
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        batchFile('XCopy C:\\Users\\kamil\\.jenkins\\workspace C:\\nginx-1.14.0\\html /s /y /exclude:C:\\Users\\kamil\\.jenkins\\workspace\\exclude.txt')
+    }
+}
